e93a1491147ed6edf6d0ecce9de81014df6147c0
Anastasia Yakunina <JohnyRuadaylo@yandex.ru>
2024-05-26 11:20:41 +0300

    пытаюсь пушить 2...

---
+++ src/main/java/pageObject/constants/Buttons.java
@@ -0,0 +1,6 @@
+package pageObject.constants;
+
+public enum Buttons {
+    ORDER_BUTTON,
+    NEXT_BUTTON;
+}
---
+++ src/main/java/pageObject/constants/ScooterColours.java
@@ -0,0 +1,6 @@
+package pageObject.constants;
+
+public enum ScooterColours {
+    BLACK_PEARL,
+    GREY_HOPELESSNESS;
+}
